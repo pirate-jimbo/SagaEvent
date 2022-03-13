120671749e38d9aaf355b9c2305be084ce1d87bc
Minhajul Islam Jim <48926546+pirate-jimbo@users.noreply.github.com>
2022-03-13 23:36:23 +0600

    Update AssemblyInfo.cs

--- SagaEvent/Properties/AssemblyInfo.cs
+++ SagaEvent/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,13 +22,13 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b8a80c06-7868-435a-abc6-ba41902145fe")]
 
-// Version information for an assembly consists of the following four values:
+/*// Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version
 //      Build Number
 //      Revision
-//
+//*/
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
